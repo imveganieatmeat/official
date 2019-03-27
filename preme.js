775f375d3e52ad46ac210d2d56e55401ccf498f3
imveganieatmeat <48937965+imveganieatmeat@users.noreply.github.com>
2019-03-26 23:45:01 -0500

    Update preme.js

--- preme.js
+++ preme.js
@@ -1,1 +1,39 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
 
+//button
+const prevB = document.querySelector('#prevB');
+const nextB = document.querySelector('#nextB');
+
+//counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+nextB.addEventListener('click',()=>{
+  if(counter >= carouselImages.length-1) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevB.addEventListener('click',()=>{
+  if(counter <= 0) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend', () =>{
+  if(carouselImages[counter].id === 'lastClone'){
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length-2;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+  if(carouselImages[counter].id === 'firstClone'){
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length-(carouselImages.length-1);
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+});
